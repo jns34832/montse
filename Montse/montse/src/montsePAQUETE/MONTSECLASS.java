fd1385ed72077da97a6bcb9febaf078ae32ca430
jns34832 <89801134+jns34832@users.noreply.github.com>
2021-08-31 20:55:13 +0200

    Add files via upload

---
+++ Montse/montse/src/montsePAQUETE/MONTSECLASS.java
@@ -0,0 +1,11 @@
+package montsePAQUETE;
+
+public class MONTSECLASS {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("");
+		System.out.println();
+	}
+
+}
